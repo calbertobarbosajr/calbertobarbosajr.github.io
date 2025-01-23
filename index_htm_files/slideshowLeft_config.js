c524bd277743d0b58f87363f4f041da9213911d8
Carlos Alberto Barbosa Junior <calbertobarbosajr@gmail.com>
2025-01-23 18:49:59 -0300

    um novo portfólio repaginado

--- index_htm_files/slideshowLeft_config.js
+++
@@ -1,9 +0,0 @@
-function xaraSwidgets_slideshowLeftConstructor(divID, data)
-{
-	xaraSwidgets_slideshowTemplateConstructor(divID, data, {
-		
-		orientation:'left',
-		originalWidth:690,
-		originalHeight:400
-	});
-}
